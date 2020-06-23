c51c9530146e3a173166757e3c8da24f01f2fbdd
marayza <gz.marayza@gmail.com>
2020-06-23 20:14:04 -0300

    new test

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -9,6 +9,9 @@ app.get('/mars', function (req, res) {
   res.send('Hello Mars!\n');
 });
 
+app.get('/testing', function (req, res) {
+  res.send('Parabéns, conseguiu!\n');
+});
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
