bd24efa9be7558e632e51cb5bc141b6a6dba0913
marayza <gz.marayza@gmail.com>
2020-07-13 06:27:49 -0300

    update version

--- version/app.js
+++ version/app.js
@@ -5,7 +5,7 @@ var response;
 
 app.get('/', function (req, res) {
 
-    response = 'This is version 1. ONLY teste' + '\n';
+    response = 'This is version 2 of app. Update now.' + '\n';
 
     //send the response to the client
     res.send(response);
